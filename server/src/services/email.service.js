bbb2c4ce8ee3df76b1890456d6bfba1dba74378e
Protik Datta <protikdatta135@gmail.com>
2026-05-01 20:37:20 +0600

    transporter add

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -3,14 +3,12 @@ const logger = require("../utils/logger");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // use TLS
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false, // sometimes helps with local network issues
   },
 });
 
